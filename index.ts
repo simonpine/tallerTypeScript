2545d6569f8924d2d29c89f4e1f2eaf9c04ea082
Simon Pineda <simonpine@Simons-MacBook-Pro-4.local>
2025-10-17 13:49:46 -0500

    Taller2 listo, me toco poner imagenes diferentes

--- index.ts
+++ index.ts
@@ -16,8 +16,28 @@ series.forEach((serie: Serie) => {
             <td>${serie.channel}</td>
             <td>${serie.seasons}</td>
         `;
+        trElement.id = `serie-row-${serie.id}`;
         seriesTbody.appendChild(trElement);
 });
 
+series.forEach((serie: Serie) => {
+    const rowElement = document.getElementById(`serie-row-${serie.id}`);
+    rowElement!.addEventListener('click', () => {
+        const detailElement = document.getElementById('serie-detail');
+        detailElement!.innerHTML = 
+        `
+            <div class="card" style="width: 18rem;">
+                    <img class="card-img-top" src="${serie.poster}" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">${serie.name}</h5>
+                        <p class="card-text"><strong>Channel:</strong> ${serie.channel}</p>
+                        <p class="card-text"><strong>Seasons:</strong> ${serie.seasons}</p>
+                        <p class="card-text">${serie.description}</p>
+                        <a href="${serie.webpage}" target="_blank" class="btn btn-primary">Visit webpage</a>
+                    </div>
+                </div>
+        `
+    });
+    })
 const averageSeasons: number = totalSeasons / series.length;
 averageRow.innerHTML = `Seasons average: ${Math.round(averageSeasons)}`;
